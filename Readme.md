4d768d8c2c46dd40e86e210b5ed6c38b2ec12395
pallavikalshetti <kannur.pallavi@outlook.com>
2025-07-02 17:19:45 -0400

    pushing updated file to remote repo

---
+++ Readme.md
@@ -0,0 +1,35 @@
+## Table of contents
+
+Dynamic Content Creation(#overview)
+  The application will allow users to add, update, and remove items dynamically while keeping track of the total price. This lab focuses on modifying elements, creating and appending new elements, and updating content dynamically using JavaScript.
+[Pallavi Kalshetti](#author)
+
+Users should be able to:
+
+- Add products to cart
+- Update/remove the items from the cart
+
+- Solution URL: [https://github.com/pallavikalshetti/dynamic-shopping-car]
+
+### Built with
+
+- HTML
+- CSS
+- DOM
+- Javascript
+
+### What I learned
+
+With this assignment, 
+- Learned how to add products dynamically in the cart using javascript event listener concepts
+- Create dynamic html tags in Javascripts
+- Learned how to use closest, queryselector concept while updating/removing items in the cart
+- Learned how to call the functions to update the amount at runtime
+- Using custom logic, filtered the item and display in the list
+
+### Challenges
+- Use inner loops to display the items in the list
+- Use innerhtml tags in javascript to create dynamic list and pass it to HTML
+- Use closest concept to identify nearest items and perform actions
+- Had to refer some forums online like stackoverflow site to understand the key concept
+
--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 const addProductButton = document.getElementById('add-product');
 const totalPriceSpan = document.getElementById('total-price');
+const errorMsg = document.getElementById('errorMsg')
 let totalPrice = 0;
-//add product to cart
 
+//add product to cart
 addProductButton.addEventListener('click', (event) => {
   const productNameInput = document.getElementById('product-name').value;
   const productPriceInput = document.getElementById('product-price').value;
-  const ul = document.getElementById('cart');
-  const li = document.createElement('li');
-
-  li.innerHTML = `
-        Product Name: ${productNameInput} , 
-        Prodcut Price: <input type="text" id="productprice" value="${productPriceInput}">
-        <button id="rm-btn" onclick="removeItem(event)">Remove</button>
-      `;
-  ul.appendChild(li);
-
-  updateTotalPrice(parseInt(productPriceInput));
+  if(productNameInput ==="" ||  productPriceInput === ""){
+    errorMsg.textContent = "Enter valid input to add items to cart!";
+  }else{
+    errorMsg.textContent = "";
+    const cart = document.getElementById('cart');
+    const cartItems = document.createElement('li');
+    cartItems.innerHTML = `
+          <div class="itemcontainer">
+            <labal> Product Name: </label>
+            <label> ${productNameInput} </label>, 
+            <labal> Product Cost: $ </label>
+            <span> ${productPriceInput} </span>
+            <button id="rm-btn" onclick="removeItem(event)">Remove</button>
+          </div>
+        `;
+    cart.appendChild(cartItems);
+    updateTotalPrice(parseInt(productPriceInput));
+  }
 })
 
 // Function to update the total price
 function updateTotalPrice(amount) {
-  totalPrice += amount;
-  //totalPriceSpan.textContent = totalPrice.toFixed(2);
+  totalPrice = totalPrice + amount;
   totalPriceSpan.textContent = totalPrice;
 }
  
 // Function to remove an item
 function removeItem(event) {
+  errorMsg.textContent = "";
   const rmButton = document.getElementById('rm-btn');
-  const rmPriceAmt = document.getElementById('productprice').value;
+  const rmcartContainer = event.target.closest('.itemcontainer');
+  const rmgetSpanAmt = rmcartContainer.querySelector('span');
+  const rmPriceAmt = rmgetSpanAmt.textContent;
   const item = event.target.closest('li');
+  //alert(rmPriceAmt);
   updateTotalPrice(-parseInt(rmPriceAmt));
   item.remove();
 }
